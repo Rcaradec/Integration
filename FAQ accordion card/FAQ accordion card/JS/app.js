911c38ed78bd832159c01f1551f28448324b0523
Rcaradec <72319136+Rcaradec@users.noreply.github.com>
2021-03-08 15:14:15 +0100

    Add files via upload

---
+++ FAQ accordion card/FAQ accordion card/JS/app.js
@@ -0,0 +1,63 @@
+
+
+
+function toggle1() {
+    let p1 = document.getElementById('p1');
+
+        if (p1.style.display === 'none') {
+            p1.style.display = 'block';
+        } else {
+            p1.style.display = 'none';
+        }
+    
+} 
+
+
+function toggle2() {
+    let p1 = document.getElementById('p2');
+
+        if (p2.style.display === 'none') {
+            p2.style.display = 'block';
+        } else {
+            p2.style.display = 'none';
+        }
+    
+} 
+
+
+function toggle3() {
+    let p3 = document.getElementById('p3');
+
+        if (p3.style.display === 'none') {
+            p3.style.display = 'block';
+        } else {
+            p3.style.display = 'none';
+        }
+    
+} 
+
+
+function toggle4() {
+    let p4 = document.getElementById('p4');
+
+        if (p4.style.display === 'none') {
+            p4.style.display = 'block';
+        } else {
+            p4.style.display = 'none';
+        }
+    
+} 
+
+
+function toggle5() {
+    let p5 = document.getElementById('p5');
+
+        if (p5.style.display === 'none') {
+            p5.style.display = 'block';
+        } else {
+            p5.style.display = 'none';
+        }
+    
+} 
+
+
